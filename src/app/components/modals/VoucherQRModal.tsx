a07553e9e4b85692db190d413091905d33d1306f
Suhan Ha <starpiung@naver.com>
2025-05-09 22:54:02 +0900

    initial implement QR modal

---
+++ src/app/components/modals/VoucherQRModal.tsx
@@ -0,0 +1,30 @@
+import { CheckCircleIcon } from "@heroicons/react/24/outline";
+import { Modal, ModalWrapper, ModalContent } from "../Modal";
+import styles from "@/styles/components/modals/VoucherQRModal.module.scss";
+import { QRCodeSVG } from "qrcode.react";
+
+interface Props {
+    showModal: boolean;
+    setModal: (open: boolean) => void;
+}
+
+const VoucherQRModal = ({ showModal, setModal }: Props) => {
+    return (
+        <Modal open={showModal} onOpenChange={setModal}>
+            <ModalWrapper>
+                <ModalContent>
+                    <div className={styles.base}>
+                        <div className={styles.contentWrapper}>
+                            <QRCodeSVG size={256} value="dd" />
+                            <h3 className={styles.timer}>03:00</h3>
+                            <p className={styles.description}>현재 화면을 직원에게 보여주세요.</p>
+                        </div>
+                        <button className={styles.submit} onClick={() => { }}>재발급</button>
+                    </div>
+                </ModalContent>
+            </ModalWrapper>
+        </Modal>
+    );
+};
+
+export default VoucherQRModal;
--- src/app/vouchers/page.tsx
+++ src/app/vouchers/page.tsx
@@ -1,5 +1,8 @@
+"use client"
 import styles from "@/styles/pages/VouchersPage.module.scss"
 import { v4 as uuid } from "uuid";
+import VoucherQRModal from "../components/modals/VoucherQRModal";
+import { useState } from "react";
 
 const data = [
     {
@@ -11,17 +14,26 @@ const data = [
 ];
 
 export default function VouchersPage() {
+    const [showQRModal, setQRModal] = useState(false);
+
+    const handleItemClick = () => {
+        setQRModal(true);
+    };
+
     return (
-        <div className={styles.base}>
-            <div className={styles.items}>
-                {data.map((d) => (
-                    <div key={d.id} className={styles.item}>
-                        <span className={styles.status}>{d.status}</span>
-                        <h3 className={styles.title}>{d.restaurantName}</h3>
-                        <p className={styles.description}>만료일자: {d.expiresAt}</p>
-                    </div>
-                ))}
+        <>
+            <VoucherQRModal setModal={setQRModal} showModal={showQRModal} />
+            <div className={styles.base}>
+                <div className={styles.items}>
+                    {data.map((d) => (
+                        <div key={d.id} className={styles.item} onClick={handleItemClick}>
+                            <span className={styles.status}>{d.status}</span>
+                            <h3 className={styles.title}>{d.restaurantName}</h3>
+                            <p className={styles.description}>만료일자: {d.expiresAt}</p>
+                        </div>
+                    ))}
+                </div>
             </div>
-        </div>
+        </>
     );
 };
