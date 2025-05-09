f09ce91f202cd530753bce7ae3a6d25283d90ce9
Suhan Ha <starpiung@naver.com>
2025-05-10 03:26:46 +0900

    disable lint, ts check for now

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    output: "standalone"
+    output: "standalone",
+    typescript: {
+        ignoreBuildErrors: true
+    },
 };
 
 export default nextConfig;
